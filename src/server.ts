e19df6205a380279cf877c6459017d62c15de675
Romis-Lab <khatiwadaromish619@gmail.com>
2026-05-15 15:28:49 +0545

    Server setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import app from "./app";
 import connectDatabase from "./config/db.config";
-import userRoutes from "./routes/user.routes"
+
 const PORT = 8080;
 const DB_URI = "mongodb://localhost:27017/team_12";
 
 //! connect database
 connectDatabase(DB_URI);
-//! users
-app.use("/api/users", userRoutes);
-//! 
-//! listening on port
+
+//! start server
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
